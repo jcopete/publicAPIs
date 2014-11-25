c94647b1f1342330bdd9e33a19d7b04f020d0fc4
Joaquin Copete <j_copete@hotmail.com>
2014-11-25 23:23:27 +0100

    added mongo url

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var express = require('express')
   , mongoose = require ('mongoose');
 
 var app = express();
+//mongodb://meetupUser:meetupPwd@ds055980.mongolab.com:55980/cdstore
+mongoose.connect('mongodb://meetupUser:meetupPwd@ds055980.mongolab.com:55980/cdstore');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -14,6 +16,7 @@ app.get('/', function(request, response) {
 
 app.get ('/cd', cds.findAll );
 
+app.get ('/store'. store.index );
 //app.put ('/cd', cds.updateCD );
 //app.post ('/cd', cds.storeCD );
 //app.get ('/cd:id', cds.findById );
--- routes/store.js
+++ routes/store.js
@@ -3,7 +3,7 @@ var Cd = require('../models/cd').Cd;
 exports.index = function(req, res) {
 Cd.find({}, function(err, docs) {
 if(!err) {
-res.json(200, { workouts: docs });
+res.json(200, { cds: docs });
 } else {
 res.json(500, { message: err });
 }
