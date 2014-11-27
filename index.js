e946f602c209e6a75d07a7a9cc42d721bd14b8f1
Joaquin Copete <j_copete@hotmail.com>
2014-11-27 15:30:05 +0100

    Added cds path

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express')
+  , bodyParser  = require("body-parser")
   , cds = require('./routes/cds')
   , store = require ('./routes/store')
   , mongoose = require ('mongoose');
@@ -8,6 +9,8 @@ var app = express();
 mongoose.connect('mongodb://meetupUser:meetupPwd@ds055980.mongolab.com:55980/cdstore');
 
 app.set('port', (process.env.PORT || 5000))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
@@ -15,9 +18,11 @@ app.get('/', function(request, response) {
 })
 
 app.get ('/cd', cds.findAll );
+app.get ('/cds', cds.findAll );
 
 app.get ('/store', store.findAll );
 app.post ('/store', store.saveCD );
+app.delete ('/store', store.deleteCD );
 //app.put ('/cd', cds.updateCD );
 //app.post ('/cd', cds.storeCD );
 //app.get ('/cd:id', cds.findById );
--- routes/store.js
+++ routes/store.js
@@ -12,8 +12,35 @@ exports.findAll = function(req, res) {
 
 
 exports.saveCD = function (req, res) {
-    var cdId= req.body.id;
+    var cdId = req.body.id;
     
-    res.json(200, { cds: cdId });
+    var newCd = new Cd();
+    newCd.id=cdId;
+    newCd.category=req.body.category;
+    newCd.name=req.body.name;
+    newCd.status=req.body.status;
+    
+    newCd.save(function(err) {
+    if(!err) {
+        res.status(201).json(201, {message: "Cd created with name: " + newCd.name });
+    } else {
+        res.status(500).json( {message: "Could not create cd. Error: " + err});
+    }
+    });
+    
+}
+
+exports.deleteCD = function (req, res) {
+    var cd_name = req.body.name;
+    Cd.findOne = ({ name: cd_name}, function (err,doc) {
+        if(!err && !doc) {
+            doc.remove();
+            res.status(200).json( { message: "CD deleted"});
+        } else if (!err) {
+            res.status(404).json( { message: "Could not find cd"});
+        } else {
+            res.status(403).json( {message: "Could not delete cd"});
+        }
+    });
 }
 
